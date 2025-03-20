f5c4c9dcd02f77baf8133d044a3c1aa4b4f702c1
ExpectationMax <1303655+ExpectationMax@users.noreply.github.com>
2025-03-20 15:20:53 +0100

    Add nice_view_gem

--- build.sh
+++ build.sh
@@ -15,3 +15,14 @@ mkdir build
 west build -s zmk/app -d build -b eyelash_corne_left -- -DZMK_CONFIG=$(pwd)/config -DSHIELD=settings_reset
 cp build/zephyr/zmk.uf2 out/left_settings_reset.uf2
 rm -r build
+
+
+mkdir build
+west build -s zmk/app -d build -b eyelash_corne_left -S studio-rpc-usb-uart -- -DZMK_CONFIG=$(pwd)/config -DSHIELD=nice_view_gem -DCONFIG_ZMK_STUDIO=y -DCONFIG_ZMK_STUDIO_LOCKING=n
+cp build/zephyr/zmk.uf2 out/left_gem.uf2
+rm -r build
+
+mkdir build
+west build -s zmk/app -d build -b eyelash_corne_right -- -DZMK_CONFIG=$(pwd)/config -DSHIELD=nice_view_gem
+cp build/zephyr/zmk.uf2 out/right_gem.uf2
+rm -r build
